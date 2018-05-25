07003f084a089580cb32dea1f7fa65117837c922
bburstei <bburstei@ebi.ac.uk>
2018-05-25 11:24:48 +0100

    trying to add login page

--- src/ui/App.jsx
+++ src/ui/App.jsx
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom';
 import Home from './Home';
 import '../styles/Gifts.css';
 import Mappings from "./Mappings";
+import Login from "./Login";
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <Switch>
         <Route exact path={'/'} component={Home}/>
         <Route exact path={'/mappings'} component={Mappings}/>
+          <Route exact path={'/login'} component={Login}/>
       </Switch>
     );
   }
