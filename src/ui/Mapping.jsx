69b97bbf0108aa78926b2b458053b4618593fafb
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2018-06-27 10:49:10 +0100

    improved mapping page

--- src/ui/Mapping.jsx
+++ src/ui/Mapping.jsx
@@ -23,7 +23,7 @@ class Mapping extends Component {
   statusOptions = {
     "NOT_REVIEWED": "Not Reviewed",
     "UNDER_REVIEW": "Under Review",
-    "REVIEWED": "Reviewd",
+    "REVIEWED": "Reviewed",
     "REJECTED": "Rejected",
   }
 
@@ -241,9 +241,20 @@ class Mapping extends Component {
     };
 
     const RelatedMapping = props => (
-      <div>
+      <div className="related-mapping">
         <Link to={`/mapping/${props.id}`}>
-          <h4>{`${props.ensgId}`}</h4>
+          <span>{props.enstId}</span>
+          <div style={{display: 'inline-block'}} dangerouslySetInnerHTML={{__html: `
+            <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                 viewBox="0 0 200.423 200.423" style="enable-background:new 0 0 200.423 200.423; width: 100px; height: 50px;" xml:space="preserve">
+              <g>
+                <polygon style="fill:#010002;" points="7.913,102.282 192.51,102.282 160.687,134.094 163.614,137.018 200.423,100.213 
+                  163.614,63.405 160.687,66.325 192.51,98.145 7.913,98.145 39.725,66.332 36.798,63.405 0,100.213 36.798,137.018 39.725,134.101  
+                  "/>
+              </g>
+            </svg>`
+          }} />
+          <span>{props.uniprotAccession}</span>
         </Link>
       </div>
     );
@@ -275,14 +286,10 @@ console.log("mapping state:", this.state);
                 : <a href="#" onClick={this.enableAddLabelMode}>Add label</a>
               }
             </div>
-{/*
-            <div style={{ height: '30vh' }}>
-              <h2>Mapping</h2>
-            </div>
- */}
+
             <div style={{marginTop: '2rem', height: '15vh'}}>
               <span style={mappingIdStyles}>
-                {`${mapping.ensemblTranscript.ensgId} (v${mapping.ensemblTranscript.enstVersion})`}
+                {`${mapping.ensemblTranscript.enstId} (v${mapping.ensemblTranscript.enstVersion})`}
               </span>
 
               <span dangerouslySetInnerHTML={{__html: `
@@ -303,7 +310,12 @@ console.log("mapping state:", this.state);
 
             <div>
               <h3>Related Mappings</h3>
-              {relatedMappings.map(mapping => <RelatedMapping id={mapping.mappingId} ensgId={mapping.ensemblTranscript.ensgId} key={mapping.mappingId} />)}
+              {relatedMappings.map(mapping => <RelatedMapping
+                id={mapping.mappingId}
+                enstId={mapping.ensemblTranscript.enstId}
+                uniprotAccession={mapping.uniprotEntry.uniprotAccession}
+                key={mapping.mappingId} />
+              )}
             </div>
 
             <div style={{marginTop: '3rem'}}>
