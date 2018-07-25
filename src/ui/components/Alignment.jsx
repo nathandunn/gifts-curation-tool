98f840b347303b56fe65361324a1d007cca178c3
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2018-07-25 13:15:47 +0100

    with initial version of the alignment visualization

--- src/ui/components/Alignment.jsx
+++ src/ui/components/Alignment.jsx
@@ -9,7 +9,10 @@ class Alignment extends Component {
     '--------MEEPMAFSSLRGSDRCPADDSLKKYEQSVKLSGIKRDIEELCEAVPQLVNVFKIKDKIGEGTFSSVYLATAQLQEGHEEKIALKHLIPTSHPMRIAAELQCLTVAGGQDNVMGLKYCFRKNDHVVIAMPYLEHESFLDILNSLSFQEVREYMYNLFVALKRIHQFGIVHRDVKPSNFLYNRRLKKYALVDFGLAQGTRDTKIELLKFVQSEAQQEDCSRNKYHGVVGHKGLLSRPAPKTVDQQCTPKTSVKRSYTQ--VHIKQGKDGKERSVGLSVQRSVFGERNFNIHSSISHESPAEKLIKQSKTVDIISRKLATKKTAISTKAMNS-VMRETARSCPAVLTCDCYGSDRVCSVCLSRRQQVAPRAGTPGFRAPEVLTKCPDQTTAIDMWSAGVIFLSLLSGRYPFYKASDDLTALAQIMTIRGSRETIQAAKAFGKSVLCSKEVPAQDLRALCERLRGLDSTTPRSASGPPGNASYDPAASKNTDHKASRVQAAQA-QHSEDSLYKRDNDGYWSHPKDCTSNSEGWDSVPDEAYDLLDKLLDLNPASRITAEAALLHAFFKDMCS'
   ];
 
-  alignmentIDs = ['O00311 CDC7_HUMAN','Q9Z0H0 CDC7_MOUSE'];
+  alignmentIDs = ['UNP','ENS'];
+  rowSize = 60;
+  positions = undefined;
+  startEndPosition = undefined;
 
   matchSequences = sequences => {
     const paired = sequences
@@ -30,54 +33,144 @@ class Alignment extends Component {
     return paired;
   }
 
+  calculatePositions = (alignments, size) => {
+
+    this.positions = alignments
+      .map(seq => {
+        let innerCounter = 0;
+
+        return seq
+          .split('')
+          .map((x, index, array) => {
+            return ('-' !== x)
+              ? array[index] = ++innerCounter
+              : null;
+          })
+      });
+
+console.log(">> Positions:", this.positions);
+
+    this.startEndPosition = alignments
+      .map(seq => {
+        return seq
+          .match(new RegExp(`.{1,${size}}`, 'g'))
+          .map(chunk => chunk.replace(/-/g, '').length)
+          .reduce((accu, current, index, array) => {
+
+              if (0 === index) {
+                accu[0][1] = current;
+
+                return accu;
+              }
+
+              accu[index] = [
+                (accu[index - 1][1]) + 1,
+                (accu[index - 1][1]) + current
+              ];
+
+            return accu;
+          }, [[1, null]]);
+      });
+
+    // console.log("positions:", this.startEndPosition);
+  }
+
   createAlignmentVisualization = () => {
+    this.calculatePositions(this.alignments, this.rowSize);
     const alignment = this.matchSequences(this.alignments);
 
-    const rowSize = 20;
     const rows = alignment
       .reduce((accu, value, index, array) => {
-        if (0 === index % rowSize) {
-          accu.push(array.slice(index, index + rowSize));
+        if (0 === index % this.rowSize) {
+          accu.push(array.slice(index, index + this.rowSize));
         }
 
         return accu;
       }, []);
 
-console.log("ROWS:", rows);
-
     return (
       <Fragment>
-        {rows.map(row => (
-          <div className="alignment__row">
-            <div className="alignment__id-wrapper">
-              {this.alignmentIDs.map(id => (
-                <div className="alignment_id">{id}</div>
-              ))}
-            </div>
+        <div id="alignment-hover-tooltip" className="alignment__hover-position">
+          <div id="positionA" className="alignment__hover-position__A"></div>
+          <div id="positionB" className="alignment__hover-position__B"></div>
+        </div>
 
-            <div className="alignment__seqs">
-              {row.map(el => {
-                let extraCSSClasses = '';
+        {rows.map((row, rowIndex) => {
+// console.log("==> row index:", index, row);
+          return (
+            <div className="alignment__row" key={`row-${rowIndex}`}>
+              <div className="alignment__id-wrapper">
+                {this.alignmentIDs.map(id => (
+                  <div className="alignment_id" key={`id-${id}:${rowIndex}`}>{id}</div>
+                ))}
+              </div>
 
-                if (el.join('') !== el[0].repeat(el.length)) {
-                  extraCSSClasses = 'alignment__changed';
+              <div className="alignment__seqs">
+                <div className="alignment__column alignment__start">
+                  <div className="alignment__position">{this.startEndPosition[0][rowIndex][0]}</div>
+                  <div className="alignment__position">{this.startEndPosition[1][rowIndex][0]}</div>
+                </div>
 
-                  if (-1 !== el.indexOf('-')) {
-                    extraCSSClasses = 'alignment__deleted';
+                {row.map((el, cellIndex) => {
+                  let extraCSSClasses = '';
+
+                  if (el.join('') !== el[0].repeat(el.length)) {
+                    extraCSSClasses = 'alignment__changed';
+
+                    if (-1 !== el.indexOf('-')) {
+                      extraCSSClasses = 'alignment__deleted';
+                    }
                   }
-                }
 
-                return (
-                  <div className={`alignment__column ${extraCSSClasses}`}>
-                    {el.map(i => <div className="alignment__cell">{i}</div>)}
-                  </div>
-                );
-              })}
+                  return (
+                    <div
+                      className={`alignment__column ${extraCSSClasses}`}
+                      onMouseEnter={this.showPosition}
+                      position-a={el[0]}
+                      position-b={el[1]}
+                      row-index={`${rowIndex}`}
+                      cell-index={`${cellIndex}`}
+                      key={`col-${rowIndex}:${cellIndex}`}
+                      >
+                      {el.map((x, i) => <div className="alignment__cell" key={`cell-${i}-${rowIndex}:${cellIndex}`}>{x}</div>)}
+                    </div>
+                  );
+                })}
+
+                <div className="alignment__column alignment__end">
+                  <div className="alignment__position">{this.startEndPosition[0][rowIndex][1]}</div>
+                  <div className="alignment__position">{this.startEndPosition[1][rowIndex][1]}</div>
+                </div>
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </Fragment>
     );
+  }
+
+  showPosition = ({ currentTarget }) => {
+    const tooltip = document.getElementById('alignment-hover-tooltip');
+    const elementA = document.getElementById('positionA');
+    const elementB = document.getElementById('positionB');
+
+    const targetPosition = currentTarget.getBoundingClientRect();
+    const positionA = currentTarget.getAttribute('position-a');
+    const positionB = currentTarget.getAttribute('position-b');
+    const rowIndex = parseInt(currentTarget.getAttribute('row-index'));
+    const cellIndex = parseInt(currentTarget.getAttribute('cell-index'));
+    const index = (rowIndex * this.rowSize) + cellIndex;
+    const valueA = this.positions[0][index];
+    const valueB = this.positions[1][index];
+
+    elementA.innerHTML = (null !== valueA) ? `${positionA}:${valueA}` : '-';
+    elementB.innerHTML = (null !== valueB) ? `${positionB}:${valueB}` : '-';
+
+    tooltip.style.top = targetPosition.y + window.scrollY - 25 + 'px';
+    tooltip.style.left = targetPosition.x + window.scrollX - 1 + 'px';
+    tooltip.style.height = targetPosition.height + 50 + 'px';
+    // tooltip.style.width = '60px';
+    tooltip.style.display = 'block';
   }
 
   render() {
