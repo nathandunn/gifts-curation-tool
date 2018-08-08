7df2c181daad055b6c600c293dd9d223d122a686
Xavier Watkins <xavier.watkins@gmail.com>
2018-08-08 11:05:33 +0100

    Sequence facet

--- src/ui/components/AlignmentIndicator.jsx
+++ src/ui/components/AlignmentIndicator.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 import '../../styles/AlignmentIndicator.css';
 
 const AlignmentIndicator = (props) => {
-  if (props.difference === null) {
+  if (props.difference === null && props.differenceName === null) {
     return null;
-  } else if (+props.difference === 0) {
+  } else if (+props.difference === 0 || props.differenceName === 'identical') {
     return (
-      <span className="alignment-indicator" title="Perfect sequence match">
-        M
+      <span className="alignment-indicator" title="Identical sequences">
+        I
       </span>
     );
-  } else if (+props.difference <= 10) {
+  } else if (+props.difference <= 5 || props.differenceName === 'small') {
     return (
       <span
         className="alignment-indicator alignment-indicator--small"
