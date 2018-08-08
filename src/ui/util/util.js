c52a9c3b63789260fbacf6da4b05c9059c607252
Xavier Watkins <xavier.watkins@gmail.com>
2018-08-08 14:30:20 +0100

    Adding stats to homepage

---
+++ src/ui/util/util.js
@@ -0,0 +1,15 @@
+export const formatLargeNumber = x => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+export const formatStatusName = (name) => {
+    switch (name) {
+        case 'UNDER_REVIEW':
+            return 'Under review';
+        case 'NOT_REVIEWED':
+            return 'Not reviewed';
+        case 'REVIEWED':
+            return 'Reviewed';
+        case 'REJECTED':
+            return 'Rejected';
+        default:
+            return 'Not reviewed';
+    }
+};
