d49fcac2e1a38fc1d7667a15b8a2b96dcfc965d8
Xavier Watkins <xavier.watkins@gmail.com>
2018-11-21 12:06:20 +0000

    Remove uglify module as webpack 4 does it by default

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,53 +3,35 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
   output: {
-    path: __dirname + '/build',
+    path: `${__dirname}/build`,
     filename: '[name].[chunkhash].js',
-    publicPath: '/gifts'
+    publicPath: '/gifts',
   },
   // devtool: 'source-map',
   optimization: {
     splitChunks: {
-      chunks: 'all'
-    }
+      chunks: 'all',
+    },
   },
   plugins: [
     new webpack.DefinePlugin({
       BASE_URL: JSON.stringify('/gifts'),
       API_URL: JSON.stringify('/gifts/api'),
       AUTH_CALLBACK_URL: JSON.stringify('http://www.ebi.ac.uk/gifts/login'),
-      READ_ONLY: JSON.stringify((process.argv.indexOf('--READ_ONLY') > -1) || false)
+      READ_ONLY: JSON.stringify(process.argv.indexOf('--READ_ONLY') > -1 || false),
     }),
     new HtmlWebPackPlugin({
-      template: __dirname + '/public/index.html',
-      filename: 'index.html'
-    }),
-    new UglifyJSPlugin({
-      uglifyOptions: {
-        ecma: 6,
-        sourceMap: false,
-        keep_fnames: false,
-        keep_classnames: true,
-        ie8: false,
-        safari10: false,
-        warnings: false,
-        compress: {
-          passes: 1
-        },
-        output: {
-          beautify: false
-        }
-      }
+      template: `${__dirname}/public/index.html`,
+      filename: 'index.html',
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    })
-  ]
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+  ],
 });
