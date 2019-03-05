458cc6cc04f397580b7ca5d55a1413f8e15611b9
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2019-03-05 13:36:40 +0000

    basic data structure for new Filter component

--- src/ui/components/Filters.jsx
+++ src/ui/components/Filters.jsx
@@ -1,81 +1,116 @@
 import React, { Fragment, Component } from 'react';
 import PropTypes from 'prop-types';
 
-import StatusIndicator from './StatusIndicator';
-
-import '../../styles/Filters.css';
-import AlignmentIndicator from './alignment/AlignmentIndicator';
+const filtersStructure = {
+  status: {
+    label: 'Status',
+    value: 'status',
+    items: {
+      not_reviewed: {
+        label: 'Not Reviewed',
+        value: 'status:NOT_REVIEWED',
+      },
+      reviewed: {
+        label: 'Reviewed',
+        value: 'status:REVIEWED',
+      },
+      uniprot_review: {
+        label: 'UniProt Review',
+        value: 'status:UNIPROT_REVIEW',
+      },
+      under_review: {
+        label: 'Under Review',
+        value: 'status:UNDER_REVIEW',
+      },
+    },
+  },
+  organism: {
+    label: 'Organism',
+    value: 'organism',
+    items: {
+      9606: {
+        label: 'Homo sapiens',
+        value: 'organism:9606',
+      },
+      10090: {
+        label: 'Mus musculus',
+        value: 'organism:10090',
+      },
+    },
+  },
+  mappings: {
+    label: 'Mappings',
+    value: 'mappings',
+    items: {
+      mapped: {
+        label: 'Mapped',
+        value: 'type:mapped',
+        items: {
+          identical: {
+            label: 'Identical',
+            value: 'alignment:identical',
+          },
+          small: {
+            label: 'Small Diff.',
+            value: 'alignment:small',
+          },
+          large: {
+            label: 'Large Diff.',
+            value: 'alignment:large',
+          },
+        },
+      },
+      partial: {
+        label: 'Partially Mapped',
+        value: 'partial',
+        items: {
+          by_gene: {
+            label: 'By Gene Symbol',
+            value: 'type:potentially_associated',
+          },
+          by_isoform: {
+            label: 'By Isoform',
+            value: 'type:related_by_isoform',
+          },
+        },
+      },
+      unmapped: {
+        label: 'Unmapped',
+        value: 'unmapped',
+      },
+    },
+  },
+  patches: {
+    label: 'Patches',
+    value: 'patches',
+    items: {
+      exclude: {
+        name: 'Exclude',
+        value: 'patches:exclude',
+      },
+      only: {
+        name: 'Only Patches',
+        value: 'patches:only'
+      },
+    },
+  },
+};
 
 class Filters extends Component {
-  static formatLabel(item, facet) {
-    if (facet.name === 'status') {
-      return (
-        <span>
-          <StatusIndicator status={item.name} /> {item.label}
-        </span>
-      );
-    } else if (facet.name === 'sequence') {
-      return (
-        <span>
-          <AlignmentIndicator differenceName={item.name} /> {item.label}
-        </span>
-      );
-    }
-    return item.label;
-  }
-
-  isActive(facet, value) {
-    return this.props.activeFacets[facet.name] === value;
-  }
 
   render() {
-    const { data } = this.props;
-
     return (
       <div className="filters">
-        {data
-          .filter(facet => facet.items.length > 0)
-          .map(facet => (
-            <Fragment key={facet.name}>
-              <h3>{facet.label}</h3>
-              <ul className="no-bullet">
-                {facet.items.map(item => (
-                  <li key={item.name}>
-                    {this.isActive(facet, item.name) ? (
-                      <a
-                        className="filter__value filter__value--active"
-                        onClick={() => this.props.removeFilter(facet.name)}
-                      >
-                        {Filters.formatLabel(item, facet)}
-                      </a>
-                    ) : (
-                      <a
-                        className="filter__value"
-                        onClick={() => this.props.addFilter(facet.name, item.name)}
-                      >
-                        {Filters.formatLabel(item, facet)}
-                      </a>
-                    )}
-                  </li>
-                ))}
-              </ul>
-            </Fragment>
-          ))}
+        Filters
       </div>
-    );
+    )
   }
 }
 
 Filters.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.shape),
-  activeFacets: PropTypes.shape,
-  removeFilter: PropTypes.func.isRequired,
-  addFilter: PropTypes.func.isRequired,
 };
 
 Filters.defaultProps = {
-  data: [],
-  activeFacets: {},
 };
 
 export default Filters;
---
+++ src/ui/components/__Filters.jsx
@@ -0,0 +1,81 @@
+import React, { Fragment, Component } from 'react';
+import PropTypes from 'prop-types';
+
+import StatusIndicator from './StatusIndicator';
+
+import '../../styles/Filters.css';
+import AlignmentIndicator from './alignment/AlignmentIndicator';
+
+class Filters extends Component {
+  static formatLabel(item, facet) {
+    if (facet.name === 'status') {
+      return (
+        <span>
+          <StatusIndicator status={item.name} /> {item.label}
+        </span>
+      );
+    } else if (facet.name === 'sequence') {
+      return (
+        <span>
+          <AlignmentIndicator differenceName={item.name} /> {item.label}
+        </span>
+      );
+    }
+    return item.label;
+  }
+
+  isActive(facet, value) {
+    return this.props.activeFacets[facet.name] === value;
+  }
+
+  render() {
+    const { data } = this.props;
+
+    return (
+      <div className="filters">
+        {data
+          .filter(facet => facet.items.length > 0)
+          .map(facet => (
+            <Fragment key={facet.name}>
+              <h3>{facet.label}</h3>
+              <ul className="no-bullet">
+                {facet.items.map(item => (
+                  <li key={item.name}>
+                    {this.isActive(facet, item.name) ? (
+                      <a
+                        className="filter__value filter__value--active"
+                        onClick={() => this.props.removeFilter(facet.name)}
+                      >
+                        {Filters.formatLabel(item, facet)}
+                      </a>
+                    ) : (
+                      <a
+                        className="filter__value"
+                        onClick={() => this.props.addFilter(facet.name, item.name)}
+                      >
+                        {Filters.formatLabel(item, facet)}
+                      </a>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </Fragment>
+          ))}
+      </div>
+    );
+  }
+}
+
+Filters.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape),
+  activeFacets: PropTypes.shape,
+  removeFilter: PropTypes.func.isRequired,
+  addFilter: PropTypes.func.isRequired,
+};
+
+Filters.defaultProps = {
+  data: [],
+  activeFacets: {},
+};
+
+export default Filters;
